8d41e993e5649486b74862b9c18a790870c1ac2e
Andrey Alekseev <ao@MacBook-Pro-Andrey.local>
2020-02-12 17:04:18 +0300

    Updates

---
+++ precache-manifest.155bb08f6803418b8178808819c324f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280ce746d17aeab2279f94c9542b870c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5516221b9247b6c32c32",
+    "url": "/monsters-rolodex/static/css/main.57a28941.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5516221b9247b6c32c32",
+    "url": "/monsters-rolodex/static/js/main.5cc0572e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
